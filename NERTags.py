1a1a79614ba620786d7068bd818580f9fcfa6ae5
shubhamvb <shubham.barhate@ves.ac.in>
2017-11-09 12:11:50 -0500

    Added comments for NERtags.py

--- NERTags.py
+++ NERTags.py
@@ -3,6 +3,8 @@ from nltk.tag.stanford import StanfordNERTagger
 #gets the NER tags for a probable answer setnence
 def get_ner_tags(answer):
 
+	#download teh stanford ner zip file and extract it .
+	#Change the directory location in the folllowing path
 	st = StanfordNERTagger('/Users/shubhambarhate/Desktop/project3/stanford-ner-2017-06-09/classifiers/english.all.3class.distsim.crf.ser.gz',
            '/Users/shubhambarhate/Desktop/project3/stanford-ner-2017-06-09/stanford-ner.jar')
 
